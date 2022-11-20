db3c3cb91d9a8d5788d6b7029690a5cfaf59992e
AdrienneMota <adriennemota97@gmail.com>
2022-11-20 01:32:55 -0400

    feat: post registries, get registries and delete session

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import express from "express"
-import { MongoClient } from "mongodb"
+import { MongoClient, ObjectId } from "mongodb"
 import joi from "joi"
 import cors from "cors"
 import dotenv from "dotenv"
 import bcrypt from "bcrypt"
 import {v4 as creatToken} from "uuid"
+import dayjs from "dayjs"
 
 const useSchema = joi.object({
     name: joi.string().max(3).max(100).required(),
     email: joi.string().email().required(),
     password: joi.string().min(1).required()
+})
+
+const registrySchema = joi.object({
+    description: joi.string().min(3).required(),
+    value: joi.number().required(),
+    type: joi.string().valid('entrada', 'saida').required()
 })
 
 //criacao api
@@ -30,6 +37,7 @@ try {
 //criando variáveis globais
 const db = mongoClient.db('dbMywallet')
 const users = db.collection('users')
+const registries = db.collection('registries')
 
 //ROTAS//////////////////////////////////////////
 
@@ -83,6 +91,75 @@ app.post('/sign-in', async (req, res) => {
     } catch (error) {
         console.log(error)
         res.sendStatus(500)
+    }
+})
+
+app.post('/registries', async (req, res) => {
+    const {authorization} = req.headers
+    const registry = req.body
+
+    try {     
+        const token = authorization?.replace('Bearer ', '')   
+        if(!token){
+            return res.status(401).send({message: 'Usuário não autorizado'})
+        }   
+    
+        const {error} = registrySchema.validate(registry, {abortEarly: false})
+        if(error){
+            const erros = error.details.map(detail => detail.message)
+            return res.status(400).send(erros)
+        }
+        
+        const sessionUser = await db.collection('sessions').findOne({token})
+        await registries.insertOne({...registry, userId: sessionUser.userId, date: dayjs().format('DD/MM')})        
+        res.sendStatus(201)
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+    
+})
+
+app.get('/registries', async (req, res) => {
+    const { authorization } = req.headers
+
+    try {
+        const token = authorization?.replace('Bearer ', '')
+        if(!token){
+            return res.status(401).send({message: "usuário não autorizado"})
+        }
+    
+        
+        const sessionUser = await db.collection('sessions').findOne({token})
+        const user = users.findOne({_id: sessionUser?.userId})
+        if(!user){
+            return res.sendStatus(401)
+        }
+
+        const registriesUser = await registries.find({userId: sessionUser.userId}).toArray()
+
+        res.send(registriesUser)
+
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(500)
+    } 
+})
+
+app.delete('/sign-out', async(req, res) => {
+    const { authorization } = req.headers
+
+    try {
+        const token = authorization?.replace('Bearer ', '')
+        if(!token){
+            return res.status(401).send({message: "usuário não autorizado"})
+        }
+
+        await db.collection('sessions').deleteOne({token})
+        res.status(200).send("deu bom")
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error)
     }
 })
 
